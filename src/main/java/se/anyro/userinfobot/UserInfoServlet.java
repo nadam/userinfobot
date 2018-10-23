dac408232c303026b92ceb077f85a2279b41bd15
Adam Nybäck <adam@anyro.se>
2018-10-23 19:11:09 +0200

    Added channel info/links and fixed error handling.

--- src/main/java/se/anyro/userinfobot/UserInfoServlet.java
+++ src/main/java/se/anyro/userinfobot/UserInfoServlet.java
@@ -10,19 +10,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import se.anyro.tgbotapi.TgBotApi;
-import se.anyro.tgbotapi.TgBotApi.ErrorListener;
+import se.anyro.tgbotapi.types.Chat;
 import se.anyro.tgbotapi.types.Message;
 import se.anyro.tgbotapi.types.Update;
 import se.anyro.tgbotapi.types.User;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 @SuppressWarnings("serial")
-public class UserInfoServlet extends HttpServlet implements ErrorListener {
+public class UserInfoServlet extends HttpServlet {
 
     private TgBotApi api;
-    private static Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private int lastFrom;
+    private int lastId;
+
+    // private static Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     public UserInfoServlet() {
         super();
@@ -35,46 +35,55 @@ public class UserInfoServlet extends HttpServlet implements ErrorListener {
 
         resp.setStatus(200);
 
-		try {
+        try {
             Update update = api.parseFromWebhook(req.getReader());
             if (!update.isMessage()) {
                 return;
             }
             Message message = update.message;
-            User forwardFrom = message.forward_from;
-            User user = forwardFrom;
-            if (forwardFrom == null) {
-                // Not forwarded so show info of the sender instead
-                user = message.from;
+            User user = message.forward_from;
+            if (user == null) {
+                if (!message.isForwardedFromChannel()) {
+                    // Not forwarded so show info of the sender instead
+                    user = message.from;
+                }
             }
+
+            if (lastFrom == message.from.id && lastId == user.id) {
+                return; // Ignore repeated message
+            }
+
             StringBuilder builder = new StringBuilder();
-            if (user.username != null) {
-                builder.append("@").append(user.username).append('\n');
+            if (user != null) {
+                if (user.username != null) {
+                    builder.append("@").append(user.username).append('\n');
+                }
+                builder.append("Id: ").append(user.id).append('\n');
+                builder.append("First: ").append(user.first_name).append('\n');
+                if (user.last_name != null) {
+                    builder.append("Last: ").append(user.last_name).append('\n');
+                }
+                if (user.language_code != null) {
+                    builder.append("Lang: ").append(user.language_code).append('\n');
+                }
+                lastFrom = message.from.id;
+                lastId = user.id;
+            } else if (message.isForwardedFromChannel()) {
+                Chat channel = message.forward_from_chat;
+                if (channel.username != null) {
+                    builder.append("@").append(channel.username).append('\n');
+                }
+                builder.append("Id: ").append(channel.id).append('\n');
+                builder.append("Title: ").append(channel.title).append('\n');
+
+                if (message.forward_from_message_id != 0) {
+                    builder.append("https://t.me/" + message.forward_from_chat.username + "/"
+                            + message.forward_from_message_id);
+                }
             }
-            builder.append("Id: ").append(user.id).append('\n');
-            builder.append("First: ").append(user.first_name).append('\n');
-            if (user.last_name != null) {
-                builder.append("Last: ").append(user.last_name).append('\n');
-            }
-            if (user.language_code != null) {
-                builder.append("Lang: ").append(user.language_code).append('\n');
-            }
-            // Separate message for easy copy/paste on mobile
-            if (message.from.id == OWNER && forwardFrom != null) {
-                api.sendMessage(OWNER, String.valueOf(user.id));
-            }
-            api.sendMessage(message.from.id, builder.toString());
+            api.sendMessage(message.from.id, builder.toString(), null, true, 0, null);
         } catch (Exception e) {
             api.debug(e);
         }
-    }
-
-    @Override
-    public void onError(int errorCode, String description) {
-        // Ignore blocked users
-        if (errorCode == 403) {
-            return;
-        }
-        api.debug(new Exception("ErrorCode " + errorCode + ", " + description));
     }
 }
